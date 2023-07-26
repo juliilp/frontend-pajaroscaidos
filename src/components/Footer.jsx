c8554d36b85414133cf95d72fade91ecf214bba3
unknown <joakig6@gmail.com>
2023-07-26 09:27:15 -0300

    some changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,27 @@
-import { BsInstagram, BsTwitter, BsYoutube } from 'react-icons/bs'
-import { FaFacebook, FaFacebookF, FaTwitter, FaYoutube } from 'react-icons/fa'
+import { BsInstagram, BsTwitter } from 'react-icons/bs'
+import { FaFacebookF } from 'react-icons/fa'
 import { AiOutlineYoutube } from 'react-icons/ai'
 import logo from '../../public/images/Logo.png'
 import Image from 'next/image'
 export default function Footer() {
-    const textStyles=' 2xl:text-2xl xl:text-xl lg:text-lg'
-    return (
-        <footer className="flex sm:gap-0 sm:justify-between flex-col  items-center h-[25rem]  sm:items-stretch  sm:flex-row w-full bg-darkgrey sm:min-h-[10rem] sm:h-[12rem]  text-lightgrey">
 
-            <div className=' w-full h-3/6 sm:h-full flex  justify-center  sm:w-4/12 relative  '>
-     
-                <Image  src={logo} alt='PajarosCaidosLogo' className='    h-[120%] sm:h-full w-auto m-auto  absolute' priority={false} quality={100} />
+    const textStyles = ' 2xl:text-2xl xl:text-xl lg:text-lg'
+    return (
+        <footer className="flex items-center h-[25rem]  bg-darkgrey  text-lightgrey flex-col 
+         sm:gap-0 sm:justify-between  sm:items-stretch  sm:flex-row w-full sm:min-h-[10rem] sm:h-[12rem]">
+
+            <div className=' w-full h-3/6  flex  justify-center relative  
+            sm:h-full  sm:w-4/12  '>
+
+                <Image src={logo} alt='PajarosCaidosLogo' priority={false} quality={100} className=' h-[120%] w-auto m-auto  absolute sm:h-full ' />
             </div>
 
-            <div className='  flex  justify-start  items-start sm:items-stretch  w-fit sm:w-6/12 md:w-7/12 lg:w-8/12  flex-col sm:justify-center gap-3 '>
+            <div className='  flex  justify-start  items-start w-fit  flex-col  gap-3 
+            sm:items-stretch  sm:w-6/12 sm:justify-center
+             md:w-7/12 
+             lg:w-8/12 '>
                 <article >
-                    <a href="https://twitter.com/pajaros_caidos" target='_blank'  className=' flex items-center gap-3 w-fit hover:text-white' title='Twitter'>
+                    <a href="https://twitter.com/pajaros_caidos" target='_blank' className=' flex items-center gap-3 w-fit hover:text-white' title='Twitter'>
                         <BsTwitter className=' text-3xl' />
                         <span className={`${textStyles}`}>
                             @pajaros_caidos
@@ -23,7 +29,7 @@ export default function Footer() {
                     </a>
                 </article>
                 <article>
-                    <a href="https://www.facebook.com/refugio.de.aves.pajaros.caidos" target='_blank'  className=' flex items-center gap-3 w-fit hover:text-white' title='Facebook'>
+                    <a href="https://www.facebook.com/refugio.de.aves.pajaros.caidos" target='_blank' className=' flex items-center gap-3 w-fit hover:text-white' title='Facebook'>
 
                         <FaFacebookF className=' text-3xl' />
                         <span className={`${textStyles}`}>
