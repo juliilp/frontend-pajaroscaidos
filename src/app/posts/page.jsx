40c27dec21f9a31f397885efe3cb89c2d26f0b2c
unknown <joakig6@gmail.com>
2023-07-29 13:03:49 -0300

    post page and comments component responsives

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,47 +1,84 @@
 "use client"
 import { BsFillSuitHeartFill, BsSuitHeart } from 'react-icons/bs'
 import styles from '../../styles/posts.module.css'
-import { useEffect, useState } from 'react'
+import { Suspense, useEffect, useState } from 'react'
 import Comentarios from '@/components/Post/Comentarios'
 import { convertirFecha } from '@/utils/auxfunctions'
 import NuestraComunidad from '@/components/NuestraComunidad/NuestraComunidad'
+
+
+
 export default function page() {
     const [like, setLike] = useState(false)
     const [posts, setPosts] = useState({});
     const [faketotallike, setFakeTotalLike] = useState(5)
-    const [comments, setComments] = useState({})
-
+    const [comments, setComments] = useState([])
+    const [postisload, setPostIsLoad] = useState(true)
+    const [responsive, setResponsive] = useState(false)
 
     useEffect(() => {
+
         const getposts = () => {
-            fetch('http://localhost:3001/api/publication/all').then(res => res.json().then(resp => {
+
+            fetch('http://localhost:3001/api/publication/all').then(res => res.json().then(resp => {//cambiar a ruta de back sin '/api/'
                 setPosts(resp.publication)
                 setFakeTotalLike(resp.publication.reactions.length)
-                setComments(resp.publication.comments[0])
+                setComments(resp.publication.comments)
             }))
         }
-        const adaptsection=document.getElementById('Uscomunitysection')
-        const post=document.getElementById('Post')
-        console.log(post.clientHeight);
-        adaptsection.style.height=post.clientHeight+'px';
+        setPostIsLoad(false)
         getposts()
-        return () => setPosts({})
+        return () => {
+            setPosts({});
+            setPostIsLoad(true)
+            setComments([{}])
+        }
     }, [])
 
+    useEffect(() => {
+        let controlate = responsive;
+        const fixstyles = () => {
+            const adaptsection = document.getElementById('Uscomunitysection')
+            const post = document.getElementById('Post')
+            adaptsection && post && (adaptsection.style.maxHeight = post.clientHeight + 'px')
+            adaptsection && post && (adaptsection.style.height = post.clientHeight + 'px')
+        }
+        const responsiveControl = () => {
+            if (window.innerWidth < 768 && !controlate) {
+
+                setResponsive(true);
+            } else if (window.innerWidth > 769 && controlate) {
+
+                setResponsive(false)
+            }
+        };
+        !controlate && fixstyles();
+        window.addEventListener("resize", responsiveControl);
+
+        return () => {
+            window.removeEventListener("resize", responsiveControl);
+        };
+    }, [responsive])
     const likepost = () => {
         setLike(!like)
         !like ? setFakeTotalLike(faketotallike + 1) : setFakeTotalLike(faketotallike - 1)
     }
 
     return (
-        <div className=" min-h-full gap-3 flex flex-col p-6 pl-14 pr-14  text-white">
+        <div className=" min-h-full gap-3 flex flex-col p-6 
+         pl-0 pr-0
+         sm:pl-2 sm:pr-2
+         md:pl-6 md:pr-6
+         lg:pl-12 lg:pr-12
+         xl:pl-14  xl:pr-14 
+         2xl:pl-20 2xl:pr-20 text-white">
             <header className="w-full h-[10rem] bg-blue-200">
-                <h1>header</h1>
+                <h2>header</h2>
             </header>
 
-            <div className="flex    min-h-[20rem] gap-3 relative bg-red-700 h-auto ">
+            <div className="flex    min-h-[20rem] gap-3 relative  h-auto ">
 
-                <main className="w-8/12 relative h-full bg-lightgray   rounded-[10px] text-[#2B2B2B] 
+                <main className="w-full lg:w-8/12 relative min-h-[40rem] h-full bg-lightgray   rounded-[10px] text-[#2B2B2B] 
                 p-8 pt-4 pb-20 flex flex-col items-center gap-7 " id='Post'>
                     <article className='flex w-full justify-end'>
 
@@ -49,16 +86,15 @@ export default function page() {
                         {/* <span className='  text-[#727272]'>{convertirFecha(posts.createdAt)}</span> */}
                     </article>
                     <article className='w-full flex justify-center'>
-                        <h1 className=" font-semibold text-4xl  w-11/12">Title</h1>
-                        {/* <h1 className=" font-semibold text-4xl  w-11/12">{posts.title}</h1> */}
+                        <h1 className=" font-semibold  sm:text-2xl md:text-3xl xl:text-4xl 2xl:text-5xl  w-11/12">Title</h1>
+                        {/* <h1 className=" font-semibold  sm:text-2xl md:text-3xl xl:text-4xl 2xl:text-5xl  w-11/12">{posts.title}</h1> */}
                     </article>
                     <div className={` w-full bg-[#c2c2c2] h-[0.7rem]  border-2 border-lightgray rounded-lg  `} style={{ filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.30))' }} />
 
-                    <article className=' w-10/12 '>
-                        <p className={`${styles.aux} text-[#020000] text-lg  `}>Pájaros Caídos lleva adelante campañas permamentes dirigidas a que la población tome conciencia de los peligros a los que están expuestas las aves, sobre todo en los centros urbanos: el golpe de un ave en vuelo sobre los grandes ventanales, la pirotecnia en las fiestas de fin de año, el uso de la gomera por parte de los niños, las fuertes lluvias, el tráfico de fauna, etc.
-                            Alentamos también a que la gente se comprometa en el cuidado de un ave en situación de vulnerabilidad: un pichón caído del nido, un ave herida o enferma, y le informamos sobre las posibles vías de acceso a una resolución del problema.
-                            Creemos también que es fundamental la colaboración de la comunidad en la difusión de estas premisas, para que lleguen a la mayor parte de la población, a través de distintos portadores, replicando las publicaciones en la web, o distribuyendo folletos y afiches en su barrio o localidad.
-                            El trabajo coordinado con otros grupos y organizaciones comunitarias, es fundamental para que la llegada de nuestros mensajes sea lo más amplia posible, y estamos abiertos a la comunicación con todos aquellos que están dispuestos a un trabajo conjunto.</p>
+                    <article className=' w-full sm:w-11/12  md:w-10/12 '>
+                        <p className={`${styles.aux} text-[#020000] sm:text-sm  md:text-base xl:text-xl 2xl:text-2xl  `}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic placeat quia commodi illum adipisci. Dignissimos debitis officiis provident eligendi commodi! Nesciunt, eligendi placeat ad maiores repellat harum laudantium voluptatum repellendus!
+                        </p>
                     </article>
 
                     <article className='  flex justify-center  w-10/12'>
@@ -67,18 +103,24 @@ export default function page() {
                         </div>
                     </article>
                     <article className='   absolute bottom-0  right-[2rem] flex items-end gap-2 text-[#E11447] font-bold'>
-                        {!like ? <BsSuitHeart className=' text-6xl  cursor-pointer ' onClick={likepost} /> :
-                            <BsFillSuitHeartFill className=' text-6xl cursor-pointer' onClick={likepost} />}
+                        {!like ? <BsSuitHeart className=' text-3xl sm:text-4xl md:text-5xl lg:text-6xl  cursor-pointer ' onClick={likepost} /> :
+                            <BsFillSuitHeartFill className=' text-3xl sm:text-4xl md:text-5xl lg:text-6xl cursor-pointer' onClick={likepost} />}
                         <span className=' text-xl'>{faketotallike}</span>
                     </article>
                 </main>
-                <section className="w-4/12  bg-lightgray h-auto overflow-auto flex-1" id='Uscomunitysection'>
-                      <NuestraComunidad />
-                </section>
+                {!responsive &&
+                    <section className=" md:w-8/12 lg:w-4/12 flex justify-center  bg-lightgray h-auto min-h-[30rem]  overflow-y-scroll " id='Uscomunitysection'>
+                        {postisload ? <h1>Cargando...</h1> : <NuestraComunidad />}
+                    </section>}
             </div >
-            <section className="w-full  bg-lightgray min-h-[10rem] p-6">
-                <Comentarios comment={comments.comment} createdAt={convertirFecha(comments.createdAt)} />
+            <section className="w-full  bg-lightgray min-h-[15rem] h-full  sm:p-6">
+                <Comentarios comments={comments} />
             </section>
+
+            {responsive &&
+                    <section className=" w-full flex justify-center  bg-lightgray h-auto min-h-[30rem] " >
+                        {postisload ? <h1>Cargando...</h1> : <NuestraComunidad />}
+                    </section>}
         </div >
     )
 }
--- src/components/Post/Comentarios.jsx
+++ src/components/Post/Comentarios.jsx
@@ -1,6 +1,7 @@
+import { convertirFecha } from "@/utils/auxfunctions"
 import { useState } from "react"
 
-export default function Comentarios({ id, comment, createdAt, userId }) {
+export default function Comentarios({ comments}) {
     const [actualComments, setActualComments] = useState({
         Antiguos: true,
         Recientes: false,
@@ -19,22 +20,30 @@ export default function Comentarios({ id, comment, createdAt, userId }) {
         })
 
     }
-    const months=['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
-    const month=createdAt.slice(3).slice(0,2)
-    const day=createdAt.slice(0,2) 
-    const year=createdAt.slice(-4)
+   
 
-    const commentmonth=` ${months[parseInt(month - 1)]}, ${day} ${year}` 
+    const commentmonth =(coments)=> {
+        const Date=convertirFecha(coments)
+        const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+        const month = Date.slice(3).slice(0, 2)
+        const day = Date.slice(0, 2)
+        const year = Date.slice(-4)
+      
+      return   ` ${months[parseInt(month - 1)]}, ${day} ${year}`
+    }
     return (
         <div className="w-full  h-full text-lettersgray flex flex-col items-center gap-3">
 
-            <section className="flex  justify-between  w-[95%] ">
+            <section className="flex flex-col items-center w-full gap-4 sm:gap-0
+            sm:flex-row  sm:items-stretch  sm:justify-between   sm:w-[95%] ">
 
-                <article className="flex justify-start items-center">
-                    <h3 className=" text-2xl font-semibold">Comentarios</h3>
+                <article className="flex w-full sm:w-fit justify-start items-end sm:items-center min-h-[3rem] sm:min-h-[3rem]">
+                    <h3 className=" text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-semibold">Comentarios</h3>
                 </article>
 
-                <article className="flex justify-between   w-4/12 items-center ">
+                <article className="flex items-center  min-h-[3rem]  justify-between w-full min-w-fit  max-w-full  
+                text-xs  min-[370px]:text-lg  md:text-base lg:text-lg  xl:text-xl 2xl:text-2xl
+                md:min-w-[20rem] sm:w-8/12  md:w-6/12  ">
                     <h4 id="Antiguos" onClick={setComments} className={`${actualComments.Antiguos && ' font-extrabold'} hover:font-extrabold cursor-pointer`}> ANTIGUOS</h4>
                     <h4 id="Recientes" onClick={setComments} className={`${actualComments.Recientes && ' font-extrabold'} hover:font-extrabold cursor-pointer`}> RECIENTES</h4>
                     <h4 id="Destacados" onClick={setComments} className={`${actualComments.Destacados && ' font-extrabold'} hover:font-extrabold cursor-pointer`}> DESTACADOS</h4>
@@ -44,26 +53,30 @@ export default function Comentarios({ id, comment, createdAt, userId }) {
 
             <div className={` w-full bg-[#c2c2c2] h-[0.3rem]  border-2 border-lightgray rounded-lg  `} style={{ filter: ' drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))' }} />
 
-            <div className=" w-[95%] flex flex-col gap-5 ">
+            {comments.length?<div className=" w-[95%] flex flex-col gap-5 ">
+                {comments?.map((i,key) =>
+                    <section className="flex flex-col  gap-4 items-start" key={key}>
+                        <article className="flex gap-4">
+                            <figure className=" rounded-full bg-black h-[2rem] w-[2rem]"></figure>
+                            <div className="flex flex-col gap-1">
+                                <h5 className=" text-xl font-medium">Nombre</h5>
+                                {/* <span className=" text-sm">Fecha</span> */}
+                                <span className=" text-sm">{commentmonth(i.createdAt)}</span>
+                            </div>
+                        </article>
 
-                <section className="flex flex-col  gap-4 items-start ">
-                    <article className="flex gap-4">
-                        <figure className=" rounded-full bg-black h-[2rem] w-[2rem]"></figure>
-                        <div className="flex flex-col gap-1">
-                            <h5 className=" text-xl font-medium">Nombre</h5>
-                            {/* <span className=" text-sm">Fecha</span> */}
-                            <span className=" text-sm">{commentmonth}</span>
-                        </div>
-                    </article>
+                        <article title="coment" className=" font-semibold w-full sm:w-8/12">
+                            {/* <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui suscipit, veniam fuga quibusdam repudiandae eveniet fugit, enim ut aspernatur quae, tempore autem ducimus dolore facilis aut expedita consequatur neque incidunt.</p> */}
+                            <p className=" break-words">{i.comment}</p>
+                        </article>
+                        <div className={`  m-auto w-full sm:w-11/12 md:w-10/12 bg-[#c2c2c2] h-[0.1rem]  border-2 border-lightgray rounded-lg  `} style={{ filter: ' drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.23))' }} />
 
-                    <article title="coment" className=" font-semibold">
-                        {/* <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui suscipit, veniam fuga quibusdam repudiandae eveniet fugit, enim ut aspernatur quae, tempore autem ducimus dolore facilis aut expedita consequatur neque incidunt.</p> */}
-                    <p>{comment}</p>
-                    </article>
-
-                </section>
-              
-            </div>
+                    </section>
+                )
+                }
+            </div>:  <div><h1>Sin comentarios</h1></div>
+            
+        }
 
         </div>
     )
