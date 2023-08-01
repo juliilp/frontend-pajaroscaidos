36f1fe457dea302660dd1ad9d9f4effa91a582e5
unknown <joakig6@gmail.com>
2023-08-01 18:31:14 -0300

    update modal responsive and validate new post

--- src/components/Foro/Modal.jsx
+++ src/components/Foro/Modal.jsx
@@ -1,5 +1,8 @@
+import { newpostvalidations } from "@/utils/auxfunctions";
 import { useState } from "react";
 import { useEffect } from "react"
+import { AiOutlinePlayCircle } from "react-icons/ai";
+import { RiImageFill } from "react-icons/ri";
 
 export default function ModalnewPost({ setvisible }) {
     useEffect(() => {
@@ -9,43 +12,87 @@ export default function ModalnewPost({ setvisible }) {
             body && (body.style.overflow = 'auto')
         }
     })
+     
 
     const [newPost, setNewPost] = useState({
         title: '',
         description: '',
         image: [''],
     })
+    const [errors,setErrors]= useState({
+        default:'campos vacíos'
+    })
+
+    const [visibleErrors,setVisibleError]=useState(false)
+
     const handleChange = (e) => {
         setNewPost({
             ...newPost,
-            [e.target.name]: [e.target.value]
+            [e.target.name]: e.target.value
         });
+
+        setErrors(newpostvalidations({
+            ...newPost,
+            [e.target.name]: e.target.value
+        }))
     }
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(newPost)
-        setvisible()
+       if('default' in errors){
+        setVisibleError(true)
+         return alert(errors.default)
+       }
+       if(Object.entries(errors).length){
+             setVisibleError(true)
+         return alert('Revisa los errores')
+       }
+       if(!Object.entries(errors).length){
+           setVisibleError(false)
+         console.log('Todo bien post completo')
+         setTimeout(()=>{
+            setvisible()
+         },5000)  
+       }
+
     }
     return (
-        <main className="bg-[#686868cc] min-h-screen fixed h-full w-full flex justify-center items-center top-0">
+        <main className="bg-[#686868cc] min-h-screen fixed h-full w-full flex justify-center items-center top-0 overflow-scroll">
 
-            <div className="relative font-baloo font-semibold h-[30rem] w-[35rem] bg-[#D9D9D9] flex flex-col justify-between ">
+            <div className="relative font-baloo font-semibold h-[35rem] max-h-[90%] w-[40rem] max-w-[97%]  bg-[#D9D9D9] flex flex-col justify-between
+             
+             md:h-[28rem] md:w-[38rem]
+             lg:h-[30rem] lg:w-[40rem]
+             xl:h-[33rem] xl:w-[43rem]
+             2xl:h-[35rem] 2xl:w-[45rem] ">
                 <button className="absolute right-0 top-0  text-2x1" onClick={setvisible}>X</button>
 
-                <form className="w-full flex flex-col gap-5  ">
-                    <section className=" w-full flex justify-start items-center bg-lightgray min-h-[3rem] ">
-                        <h1>Crea tu post</h1>
+                <form className="w-full flex flex-col gap-5   h-5/6 overflow-y-auto ">
+                    <section className=" w-full flex justify-start items-center bg-lightgray min-h-[4rem] ">
+                        <h1 className=" text-2xl ml-4">Crea tu post!</h1>
                     </section>
                     <section className="w-full">
                         <input onChange={handleChange} value={newPost.title} className="p-3 w-full" type="text" name="title" placeholder="Agrega un titulo (obligatorio)" />
                     </section>
                     <section className="w-full">
-                        <textarea onChange={handleChange} value={newPost.description} className="w-full p-3 resize-none h-[10rem] " name="description" id="" cols="30" rows="10" placeholder="Aquí tú posteo (obligatorio)" />
+                        <textarea onChange={handleChange} value={newPost.description} className="w-full p-3 resize-none h-[15rem] md:h-[10rem] max-lg:landscape:max-h-[5rem]    " name="description" id="" cols="30" rows="10" placeholder="Aquí tú posteo (obligatorio)" />
                     </section>
 
-                    <section className="flex w-full items-center justify-center gap-8">
-                        <article><span>Imagen</span></article>
-                        <article><span>Video</span></article>
+                    <section className="flex  w-full items-center h-[25%] justify-center gap-8">
+                        <article>
+                            <input type="file" name="" id="selectimage" className="hidden" accept="image/*" />
+                            <label htmlFor="selectimage" className=" flex items-center gap-2" >
+                                <RiImageFill className="cursor-pointer text-2xl " />
+                                <span className="cursor-pointer text-[#989898] ">Imagen</span>
+                            </label>
+                        </article>
+
+                        <article>
+                            <input type="file" name="" id="SelectVideo" className="hidden" accept="video/*" />
+                            <label htmlFor="SelectVideo" className="flex items-center gap-2 ">
+                                <AiOutlinePlayCircle className="cursor-pointer text-2xl" />
+                                <span className="cursor-pointer text-[#989898]">Video</span>
+                            </label>
+                        </article>
                     </section>
 
                 </form>
--- src/utils/auxfunctions.js
+++ src/utils/auxfunctions.js
@@ -1,14 +1,24 @@
-export const  convertirFecha=(fechaapi)=> {
-    const fecha = new Date(fechaapi);
-    const dia = fecha.getDate();
-    const mes = fecha.getMonth() + 1;
-    const anio = fecha.getFullYear();
-  
-    const diaStr = dia.toString().padStart(2, '0');
-    const mesStr = mes.toString().padStart(2, '0');
-  
-    return `${diaStr}/${mesStr}/${anio}`;
-  }
-  
+export const convertirFecha = (fechaapi) => {
+  const fecha = new Date(fechaapi);
+  const dia = fecha.getDate();
+  const mes = fecha.getMonth() + 1;
+  const anio = fecha.getFullYear();
 
+  const diaStr = dia.toString().padStart(2, "0");
+  const mesStr = mes.toString().padStart(2, "0");
+
+  return `${diaStr}/${mesStr}/${anio}`;
+};
+
+export const newpostvalidations = (newpost) => {
+  let errors = {};
+  if (!newpost.title.length) errors.title = "Titulo obligatorio";
+  if (newpost.title.length < 3) errors.title = "Titulo muy corto";
+
+  if (!newpost.description.length) errors.description = "El campo de texto esta vacio";
+  if (newpost.description.length < 10) errors.description = "El texto es muy corto";
+
+  if (Object.entries(errors).length) errors.general = 'Revisa los errores por favor'
   
+  return errors
+};
