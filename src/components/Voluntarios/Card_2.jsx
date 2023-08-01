367255cccab97089868554432a61c9a42533c235
ArmandBGarcia <xdarcx@hotmail.es>
2023-08-01 16:25:07 -0600

    maquetado de voluntarios

--- src/components/Voluntarios/Card_2.jsx
+++ src/components/Voluntarios/Card_2.jsx
@@ -5,7 +5,7 @@ export const Card_2 = ({ user }) => {
   const { name, description, photo } = user;
 
   return (
-    <div>
+    <div className="hover:animate-rotate-y animate-once animate-duration-[2000ms] animate-ease-in cursor-pointer">
       {/* <span>{name}</span> */}
       <Image src={photo} alt="voluntario" />
       {/* <p>{description}</p> */}
