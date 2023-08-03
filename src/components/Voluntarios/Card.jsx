a2e95048d709db00fe75ab0a2cd6ef174332b015
ArmandBGarcia <xdarcx@hotmail.es>
2023-08-02 19:32:02 -0600

    flip card animation

--- src/components/Voluntarios/Card.jsx
+++ src/components/Voluntarios/Card.jsx
@@ -1,12 +1,26 @@
 import Image from "next/image";
+import React from "react";
+import photo from "../../../public/images/fotos.svg";
+import s from "./styles/Card.module.css";
 
-export const Card = ({ user }) => {
-  const { description, photo } = user;
+export const Card = ({ name }) => {
   return (
-    <div className="flex w-169 h-42 rounded-lg shadow-xl p-4">
-      {/* <span className="float float-left">{name}</span> */}
-      <Image src={photo} alt="voluntario" width={80} height={32} />
-      <p className=" text-sm font-roboto text-justify p-2">{description}</p>
+    <div className={s.flip_card}>
+      <div className={s.flip_card_inner}>
+        <div className={s.flip_card_front}>
+          <Image src={photo} alt="avatar" />
+        </div>
+        <div className={s.flip_card_back}>
+          <p className={s.name}>{name}</p>
+          <p className={s.description}>
+            Soy una persona apasionada y deversa en mis intereses. Me encanta
+            dedicar mi tiempo libre a salvar aves heridas, brindándoles cuidado
+            y protección. También soy programador, y la creatividad que me
+            brinda la programación complementa mi amor por la naturaleza y me
+            ayuda a encontrar soluciones innovadoras en ambos ámbitos.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
