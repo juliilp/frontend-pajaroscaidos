9c1aa6f5b02dc5e18a1384f0c657bef8be86d8ca
unknown <joakig6@gmail.com>
2023-08-04 15:47:48 -0300

    loading view, temporary

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+export default function loading() {
+    return (
+        <div className=" min-h-screen text-white bg-[#0000009d] flex flex-col items-center justify-center ">
+            <h1 className="tex-4xl">
+                CARGANDO..... :D Espere un segundito....
+            </h1>
+        </div>
+    )
+}
