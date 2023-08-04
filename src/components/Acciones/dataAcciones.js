3ff18d2118f19346d5bcd83f3f4df071f8df3760
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-08-04 18:03:55 -0300

    acciones

---
+++ src/components/Acciones/dataAcciones.js
@@ -0,0 +1,25 @@
+import PrimerImagen from "@/../public/Acciones/PrimerImagen.png";
+import SegundaImagen from "@/../public/Acciones/SegundaImagen.png";
+import TercerImagen from "@/../public/Acciones/TercerImagen.png";
+export const dataAcciones = [
+  {
+    id: 1,
+    titulo: "GEGE VOLVIO A LA SELVA",
+    texto: `El 17 de diciembre de 2019 el Refugio de la Asociación recibió un Tucán que había sido comprado ilegalmente, con el fin de ser devuelto a su hábitat natural. En razón de ello, se le dio en primera instancia atención veterinaria para comocer su estado de salud, y luego de ello, se planificó su vuelta a la selva en forma inmediata. Ante esta situación, la Asociación se contactó con la Reserva GuiraOga que se ubica en Puerto Iguazú, Provincia de Misiones, quienes aceptaron recibir al Tucán para su evaluación y plan de reinserción en la naturaleza.El 7 de enero de 2019 se inició el viaje via terrestre y 8 de enero Gege fue entregado en la Reserva GuiraOga, donde lo preparan para su liberación en la selva.`,
+    image: PrimerImagen,
+  },
+  {
+    id: 2,
+    titulo: "FERIA DE VILLA DOMINICO",
+    texto:
+      "El día 21 de diciembre de 2014 sería el comienzo de la caída de uno de los máximos exponentes en la venta de aves: la Feria De Villa Domínico, situada en la zona sur del conurbano bonaerense, lugar de asidua concurrencia de las personas que no tienen reparos en comprar y vender ejemplares de aves víctimas del tráfico de fauna, actividad prohibida por leyes y ordenanzas, y repudiada por gran parte de la comunidad.La Asociación Civil de Ayuda a las Aves Pájaros Caídos fue convocada para salvaguardar las aves que allí se encontraran, por lo que concurrimos con nuestro equipo de rescate, acompañando los allanamientos. Colaboramos en el traslado e inventario de las aves, asistiéndolas de manera tal de evitarles en lo posible, el estrés que estas situaciones producen.",
+    image: SegundaImagen,
+  },
+  {
+    id: 3,
+    titulo: "PIGEON CLUB ARGENTINO",
+    texto:
+      "A pesar que los torneos o competencias de tiro al pichón se encuentran prohibidos por la Ley Nacional Nº 14.346 por ser un acto de crueldad contra las palomas y por la Ley Nº 11.406 de la Provincia de Buenos Aires, los mismos se seguían llevando a cabo en la localidad de Ingeniero Maschwitz, Partido de Escobar. Los torneos de tiro al pichón eran organizados por el Pigeon Club Argentino, una organización de tiradores que seguía llevando adelante competencias con la complicidad de autoridades policiales y judiciales, hasta que en el año 2014, entró en acción Pajaros Caídos. Luego de varias denuncias penales que la Fiscalía de Escobar desestimó, se inició una acción de amparo que suspendió los torneos por más de un año, y desde diciembre de 2015, quedaron definitivamente prohibidos en el ámbito de la Provincia de Buenos Aires.",
+    image: TercerImagen,
+  },
+];
