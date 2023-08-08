bf7fcb145a277775804cbbe45a8b611927edeee5
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-08-08 14:35:06 -0300

    changes in foro component

--- src/components/CardForo.jsx
+++ src/components/CardForo.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import { IoMdHeartEmpty } from "react-icons/io";
-import { RiMessage2Line } from "react-icons/ri";
-export default function CardForo({ titulo, tiempo, user, like, message }) {
+import React from 'react'
+import { IoMdHeartEmpty } from 'react-icons/io'
+import { RiMessage2Line } from 'react-icons/ri'
+export default function CardForo({ titulo, tiempo, user, like, message, image }) {
   return (
     <div className=" flex w-[95%] h-[150px]  items-center justify-center shadow-cardForo sm:shadow-none sm:border-b sm:border-white ">
       {/* Supuesta imagen */}
       <div className=" flex-[2] h-full w-2/6 p-3 justify-center items-center flex ">
-        <div className="bg-slate-600 w-[100%] max-w-[80px] h-[70px] sm:max-w-[120px] sm:h-[100px] lg:max-w-[150px] lg:h-[120px] rounded-2xl" />
+        <div className="bg-slate-600 w-[100%] max-w-[80px] h-[70px] sm:max-w-[120px] sm:h-[100px] lg:max-w-[150px] lg:h-[120px] rounded-2xl">
+          <img src={image} />
+        </div>
       </div>
 
       <div className="w-4/6 h-[100px] flex flex-col px-3 gap-1 justify-center font-inter ">
@@ -17,27 +19,15 @@ export default function CardForo({ titulo, tiempo, user, like, message }) {
         </span>
         <div className="flex gap-6 self-end sm:self-start pr-6">
           <div className="flex gap-2 ">
-            <IoMdHeartEmpty
-              color="#E11447"
-              size={25}
-              className="cursor-pointer"
-            />
-            <span className="text-[#AA153A] font-semibold text-lg ">
-              {like}
-            </span>
+            <IoMdHeartEmpty color="#E11447" size={25} className="cursor-pointer" />
+            <span className="text-[#AA153A] font-semibold text-lg ">{like}</span>
           </div>
           <div className="flex  gap-2">
-            <RiMessage2Line
-              color="#0C6410"
-              size={25}
-              className="cursor-pointer"
-            />
-            <span className="text-[#0C6410] font-semibold text-lg self-end ">
-              {message}
-            </span>
+            <RiMessage2Line color="#0C6410" size={25} className="cursor-pointer" />
+            <span className="text-[#0C6410] font-semibold text-lg self-end ">{message}</span>
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
