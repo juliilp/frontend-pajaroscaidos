32d19f6cd3461513187ba5ecd33c3bfa96e3d7b5
unknown <joakig6@gmail.com>
2023-08-09 10:29:06 -0300

    about us

---
+++ src/components/Nosotros/AboutSection4.jsx
@@ -0,0 +1,10 @@
+export default function AboutSection4() {
+    return (
+        <>
+            <h1 className="m-auto">image1</h1>
+            <h1 className="m-auto">image1</h1>
+            <h1 className="m-auto">image1</h1>
+            <h1 className="m-auto">image1</h1>
+            <h1 className="m-auto">image1</h1>
+        </>)
+}
