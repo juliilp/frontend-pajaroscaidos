b1ec8db847d3c9ec6fa48841bf6f725b2a9f724c
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-08-16 22:42:17 -0300

    pagina Home

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,38 @@
-import Campañas from "@/components/Home/Campañas";
-import MainHome from "@/components/Home/MainHome";
-import NuestraComunidad from "@/components/NuestraComunidad/NuestraComunidad";
-import React from "react";
+'use client'
+import Campañas from '@/components/Home/Campañas'
+import MainHome from '@/components/Home/MainHome'
+import NuestraComunidad from '@/components/NuestraComunidad/NuestraComunidad'
+import api from '@/api/api'
+import React, { useState, useEffect } from 'react'
 
-export default function page() {
+export default function Home() {
+  const [images, setImages] = useState([])
+
+  useEffect(() => {
+    async function getBannerImages() {
+      try {
+        const response = await api.get(`/news/banner`)
+        // console.log('ress: ', response.data)
+        setImages(response.data.images)
+      } catch (error) {
+        console.log('error al obtener las noticias: ', error)
+      }
+    }
+
+    getBannerImages()
+  }, [])
+
   return (
     <section>
-      <MainHome />
-      <div className="w-full flex flex-col gap-6 xl:flex-row xl:gap-0 bg-[#d8d9d8] ">
-        <div className="w-full h-max flex flex-col gap-6 md:grid  md:grid-cols-2 2xl:grid-cols-3 ">
-          <Campañas
-            fecha="Julio 18, 2023"
-            texto="La Asociación promueve en forma permanente campañas de rescate, de cocientización, de cuidado y respeto hacia las aves y los animales. Muchas se difunden por redes sociales y llegan a todas partes, y algunas se realizan en forma presencial en la via publica con la participación de voluntarios. Vos podes sumarte y difundir nuestras campañas."
-          />
-          <Campañas
-            fecha="Julio 18, 2023"
-            texto="La Asociación promueve en forma permanente campañas de rescate, de cocientización, de cuidado y respeto hacia las aves y los animales. Muchas se difunden por redes sociales y llegan a todas partes, y algunas se realizan en forma presencial en la via publica con la participación de voluntarios. Vos podes sumarte y difundir nuestras campañas."
-          />
-          <Campañas
-            fecha="Julio 18, 2023"
-            texto="La Asociación promueve en forma permanente campañas de rescate, de cocientización, de cuidado y respeto hacia las aves y los animales. Muchas se difunden por redes sociales y llegan a todas partes, y algunas se realizan en forma presencial en la via publica con la participación de voluntarios. Vos podes sumarte y difundir nuestras campañas."
-          />
-          <Campañas
-            fecha="Julio 18, 2023"
-            texto="La Asociación promueve en forma permanente campañas de rescate, de cocientización, de cuidado y respeto hacia las aves y los animales. Muchas se difunden por redes sociales y llegan a todas partes, y algunas se realizan en forma presencial en la via publica con la participación de voluntarios. Vos podes sumarte y difundir nuestras campañas."
-          />
+      <MainHome banner={images} />
+
+      <div className="w-full flex flex-col gap-6 lg:flex-row lg:gap-0">
+        <Campañas />
+
+        <div className="lg:w-[30%] lg:ml-6">
+          <NuestraComunidad />
         </div>
-        <NuestraComunidad />
       </div>
     </section>
-  );
+  )
 }
