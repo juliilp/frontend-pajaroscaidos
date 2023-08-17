ba547c03fc17b3547f7ad6d12a103ef3791f2626
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-08-16 22:43:09 -0300

    logica de banners

--- src/components/Home/MainHome.jsx
+++ src/components/Home/MainHome.jsx
@@ -1,29 +1,21 @@
-"use client";
-import React, { useRef, useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import "../../app/swiper.css";
-import { Navigation } from "swiper/modules";
+'use client'
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import '../../app/swiper.css'
+import { Navigation } from 'swiper/modules'
 
-export default function MainHome() {
+export default function MainHome({ banner }) {
   return (
-    <>
-      <Swiper
-        navigation={true}
-        modules={[Navigation]}
-        className="mySwiper h-[300px]"
-      >
-        <SwiperSlide className="swiper-slide-centered">Slide 1</SwiperSlide>
-        <SwiperSlide className="swiper-slide-centered">Slide 2</SwiperSlide>
-        <SwiperSlide className="swiper-slide-centered">Slide 3</SwiperSlide>
-        <SwiperSlide className="swiper-slide-centered">Slide 4</SwiperSlide>
-        <SwiperSlide className="swiper-slide-centered">Slide 5</SwiperSlide>
-        <SwiperSlide className="swiper-slide-centered">Slide 6</SwiperSlide>
-        <SwiperSlide className="swiper-slide-centered">Slide 7</SwiperSlide>
-        <SwiperSlide className="swiper-slide-centered">Slide 8</SwiperSlide>
-        <SwiperSlide className="swiper-slide-centered">Slide 9</SwiperSlide>
+    <div className="mySwiperContainer h-[350px] max-h-[600px] z-[-1]">
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper h-full">
+        {banner.map((e) => (
+          <SwiperSlide key={e.id} className="swiper-slide-centered">
+            <img src={e.image} alt={`Banner ${e.name}`} className="w-full h-full object-cover" />
+          </SwiperSlide>
+        ))}
       </Swiper>
-    </>
-  );
+    </div>
+  )
 }
