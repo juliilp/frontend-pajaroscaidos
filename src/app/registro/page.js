60140a6a3079b04c04906919c9fb4eb28d0feb47
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-08-16 22:47:03 -0300

    registro

--- src/app/registro/page.js
+++ src/app/registro/page.js
@@ -1,51 +1,64 @@
 'use client'
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { AiOutlineEye } from 'react-icons/ai'
 import { AiOutlineEyeInvisible } from 'react-icons/ai'
-import LoginImagen from '../../assets/login-imagen.jpg'
+import RegistroImagen from '../../assets/registro-login.png'
 import Image from 'next/image'
 import { customContext } from '@/store/ContextProvider'
 import api from '@/api/api'
 import { useRouter } from 'next/navigation'
 import { validateCreateUser } from '@/utils/auxfunctions'
+import Cookies from 'js-cookie'
 
-export default function Registro() {
+export default function Page() {
   const router = useRouter()
-  const { setUser } = customContext()
-  const [switchPassword, setSwitchPassword] = useState(false)
+  const { setNewUserId } = customContext()
   const [errors, setErrors] = useState({})
   const [emailUsed, setEmailUsed] = useState(false)
-  const [input, setInput] = useState({
+  const [isRegistering, setIsRegistering] = useState(false)
+
+  const [switchPassword, setSwitchPassword] = useState(false)
+  const handlerSwitchPassword = () => {
+    setSwitchPassword((prev) => !prev)
+  }
+  const [formRegister, setFormRegister] = useState({
     email: '',
     password: '',
     passwordConfirm: '',
     first_name: '',
     last_name: '',
   })
-
-  const inputHandler = (e) => {
-    setInput({
-      ...input,
+  const handlerRegistro = (e) => {
+    setFormRegister({
+      ...formRegister,
       [e.target.name]: e.target.value,
     })
   }
-  const submitHandler = async (e) => {
+  const handlerSubmit = async (e) => {
     e.preventDefault()
 
-    const validationErrors = validateCreateUser(input)
+    const validationErrors = validateCreateUser(formRegister)
     if (Object.keys(validationErrors).length > 0) {
       setErrors(validationErrors)
       return
     }
 
+    if (isRegistering) {
+      return
+    }
+
+    setIsRegistering(true)
+
     try {
-      const { passwordConfirm, ...userData } = input
+      const { passwordConfirm, ...userData } = formRegister
       const response = await api.post(`/user/create`, userData, { withCredentials: true })
 
       if (response.status == 200) {
         const user = await response.data
         // console.log('newUser', user.newUser)
-        setUser(user.newUser)
+
+        Cookies.set('newUserId', JSON.stringify({ id: user.newUser.id }), { expires: 7 })
+
         router.push('/emailcode')
       }
     } catch (error) {
@@ -55,36 +68,37 @@ export default function Registro() {
           setEmailUsed(true)
         }
       }
+    } finally {
+      setIsRegistering(false)
     }
 
-    setInput({
+    setFormRegister({
       email: '',
       password: '',
       passwordConfirm: '',
       first_name: '',
       last_name: '',
     })
-
-    // console.log(input)
   }
-
   return (
-    <section className="w-full bg-[#D9D9D9] h-screen flex justify-center items-center md:grid md:grid-cols-2">
-      <Image src={LoginImagen} className="hidden md:block justify-self-end" alt="imagen" />
+    <section className="w-full h-screen flex justify-center items-center gap-12 px-12">
+      <Image src={RegistroImagen} alt="imagen" className="hidden md:block justify-self-end" />
       <form
-        className="w-[550px]  bg-[#FFFFFF] pb-16 rounded-xl font-baloo "
-        onSubmit={submitHandler}
+        className="w-full max-w-[550px] md:max-w-[400px] lg:max-w-[550px] flex flex-col bg-white rounded-xl gap-6 md:gap-8 lg:gap-10 justify-self-start"
+        onSubmit={handlerSubmit}
       >
-        <h2 className="font-bold  text-2xl text-center mt-2 mb-12">Registrate</h2>
+        <h2 className="w-full text-center mt-6 md:mt-8 lg:mt-10 font-bold text-xl md:text-2xl lg:text-3xl">
+          Registrate
+        </h2>
 
-        <div className="flex flex-col px-4 gap-2 ">
-          <span className="text-[#525252]">Nombre</span>
+        <div className="flex flex-col gap-1 mx-4">
+          <span>Nombre</span>
           <input
             type="text"
-            className="py-3 outline-none pl-2 bg-[#D9D9D9] "
-            onChange={inputHandler}
+            className="bg-[#EEEEEE] outline-none py-3 pl-1"
             name="first_name"
-            value={input.first_name}
+            onChange={handlerRegistro}
+            value={formRegister.first_name}
           />
           <span
             className="text-red-500"
@@ -94,14 +108,14 @@ export default function Registro() {
           </span>
         </div>
 
-        <div className="flex flex-col px-4 gap-2 ">
-          <span className="text-[#525252]">Apellido</span>
+        <div className="flex flex-col gap-1 mx-4">
+          <span>Apellido</span>
           <input
             type="text"
-            className="py-3 outline-none pl-2 bg-[#D9D9D9]"
-            onChange={inputHandler}
+            className="bg-[#EEEEEE] outline-none py-3 pl-1"
             name="last_name"
-            value={input.last_name}
+            onChange={handlerRegistro}
+            value={formRegister.last_name}
           />
           <span
             className="text-red-500"
@@ -111,14 +125,14 @@ export default function Registro() {
           </span>
         </div>
 
-        <div className="flex flex-col px-4 gap-2 ">
-          <span className="text-[#525252]">Email</span>
+        <div className="flex flex-col gap-1 mx-4">
+          <span>Email</span>
           <input
-            type="text"
-            className="py-3 outline-none pl-2 bg-[#D9D9D9]"
-            onChange={inputHandler}
+            type="email"
+            className="bg-[#EEEEEE] outline-none py-3 pl-1"
             name="email"
-            value={input.email}
+            onChange={handlerRegistro}
+            value={formRegister.email}
           />
           <span
             className="text-red-500"
@@ -128,46 +142,48 @@ export default function Registro() {
           </span>
         </div>
 
-        <div className="flex flex-col px-4 gap-2 ">
-          <span className="text-[#525252]">Contraseña</span>
-          <div className="flex relative ">
+        <div className="flex flex-col gap-1 mx-4">
+          <span>Contraseña</span>
+          <div className="relative">
             <input
               type={switchPassword ? 'text' : 'password'}
-              className="py-3  w-full outline-none  pl-2 bg-[#D9D9D9]"
-              onChange={inputHandler}
+              className="bg-[#EEEEEE] outline-none py-3 pl-1 w-full"
               name="password"
-              value={input.password}
+              onChange={handlerRegistro}
+              value={formRegister.password}
             />
-            <div
-              className="absolute right-0 top-[18%]"
-              onClick={() => setSwitchPassword((prev) => !prev)}
-            >
-              {switchPassword ? <AiOutlineEye size={30} /> : <AiOutlineEyeInvisible size={30} />}
+            <div className="absolute top-[18%] right-0">
+              {switchPassword ? (
+                <AiOutlineEye size={30} onClick={handlerSwitchPassword} />
+              ) : (
+                <AiOutlineEyeInvisible size={30} onClick={handlerSwitchPassword} />
+              )}
             </div>
+            <span
+              className="text-red-500"
+              style={{ visibility: errors.password ? 'visible' : 'hidden' }}
+            >
+              {errors.password}
+            </span>
           </div>
-          <span
-            className="text-red-500"
-            style={{ visibility: errors.password ? 'visible' : 'hidden' }}
-          >
-            {errors.password}
-          </span>
         </div>
 
-        <div className="flex flex-col px-4 gap-2 ">
-          <span className="text-[#525252]">Confirmar Contraseña</span>
-          <div className="flex relative ">
+        <div className="flex flex-col gap-1 mx-4">
+          <span>Confirmar Contraseña</span>
+          <div className="relative">
             <input
               type={switchPassword ? 'text' : 'password'}
-              className="py-3  w-full outline-none  pl-2 bg-[#D9D9D9]"
-              onChange={inputHandler}
+              className="bg-[#EEEEEE] outline-none py-3 pl-1 w-full"
               name="passwordConfirm"
-              value={input.passwordConfirm}
+              onChange={handlerRegistro}
+              value={formRegister.passwordConfirm}
             />
-            <div
-              className="absolute right-0 top-[18%]"
-              onClick={() => setSwitchPassword((prev) => !prev)}
-            >
-              {switchPassword ? <AiOutlineEye size={30} /> : <AiOutlineEyeInvisible size={30} />}
+            <div className="absolute top-[18%] right-0">
+              {switchPassword ? (
+                <AiOutlineEye size={30} onClick={handlerSwitchPassword} />
+              ) : (
+                <AiOutlineEyeInvisible size={30} onClick={handlerSwitchPassword} />
+              )}
             </div>
           </div>
           <span
@@ -178,14 +194,23 @@ export default function Registro() {
           </span>
         </div>
 
-        <div className="flex flex-col  justify-center items-center my-10">
-          <button
-            type="submit"
-            className="bg-[#008000] h-[70px] w-[180px] font-bold text-[#FFFFFF] text-lg rounded-xl "
-          >
-            Crear cuenta
-          </button>
-        </div>
+        {/*<div className="flex flex-col gap-1 mx-4">
+          <span>Tipo de voluntario</span>
+          <input
+            type="text"
+            className="bg-[#EEEEEE] outline-none py-3 pl-1"
+            name="voluntario"
+            onChange={handlerRegistro}
+            value={formRegister.voluntario}
+          />
+        </div>*/}
+
+        <button
+          className="bg-[#128117] text-white px-16 py-5 w-max mx-auto mb-4"
+          disabled={isRegistering}
+        >
+          {isRegistering ? 'Registrando...' : 'Registrate'}
+        </button>
       </form>
     </section>
   )
