bae71d0c0f624f78a0fa0449f95ac80983c3466f
Felipe Moriones <96366130+Moriones20@users.noreply.github.com>
2023-08-19 19:08:40 -0500

    Efecto menu desktop

--- src/components/Navbar/MenuDesktop.jsx
+++ src/components/Navbar/MenuDesktop.jsx
@@ -17,7 +17,7 @@ export default function MenuDesktop() {
   };
   return (
     <ul className="flex gap-8 text-white bg-[#3D3D3D] lg:gap-16 ">
-      <li className=" cursor-pointer justify-center items-center flex ">
+      <li className=" cursor-pointer justify-center items-center flex text-white hover:text-gray-300 focus:outline-none ">
         <Link href="/">Inicio</Link>
       </li>
       <li
@@ -25,7 +25,7 @@ export default function MenuDesktop() {
         data-menu="asociacion"
       >
         <span
-          className="flex items-center gap-2"
+          className="flex items-center gap-2 text-white hover:text-gray-300 focus:outline-none"
           onClick={(e) => handlerMenu(e, "asociacion")}
         >
           Asociación
@@ -36,34 +36,38 @@ export default function MenuDesktop() {
             } duration-200`}
           />
         </span>
-        {switchMenu.asociacion && (
-          <div className="absolute top-14 -left-5">
-            <MenuInterno
-              opciones={[
-                <Link key="opcion1" href="/acciones">
-                  Acciones
-                </Link>,
-                <Link key="opcion2" href="/caza-trafico">
-                  Caza y tráfico de faunas
-                </Link>,
-                <Link key="opcion3" href="/actosmaltratoycrueldad">
-                  Crueldad y maltrato
-                </Link>,
-                <Link key="opcion4" href="/legislacion">
-                  Legislación
-                </Link>,
-              ]}
-              tamaño="md:w-[100%] lg:w-[115%]  md:py-4 md:rounded-br-[10px] md:rounded-bl-[10px]"
-            />
-          </div>
-        )}
+        <div
+          className={`absolute top-14 -left-5 transition-all duration-300 -z-20 ${
+            switchMenu.asociacion
+              ? "translate-y-0 opacity-100"
+              : "-translate-y-12 opacity-0"
+          }`}
+        >
+          <MenuInterno
+            opciones={[
+              <Link key="opcion1" href="/acciones">
+                Acciones
+              </Link>,
+              <Link key="opcion2" href="/caza-trafico">
+                Caza y tráfico de faunas
+              </Link>,
+              <Link key="opcion3" href="/actosmaltratoycrueldad">
+                Crueldad y maltrato
+              </Link>,
+              <Link key="opcion4" href="/legislacion">
+                Legislación
+              </Link>,
+            ]}
+            tamaño="md:w-[100%] lg:w-[115%]  md:py-4 md:rounded-br-[10px] md:rounded-bl-[10px]"
+          />
+        </div>
       </li>
       <li
         className="flex gap-1 items-center cursor-pointer relative"
         data-menu="comunidad"
       >
         <span
-          className="flex items-center gap-2"
+          className="flex items-center gap-2 text-white hover:text-gray-300 focus:outline-none"
           onClick={(e) => handlerMenu(e, "comunidad")}
         >
           Comunidad
@@ -74,28 +78,32 @@ export default function MenuDesktop() {
             } duration-200`}
           />
         </span>
-        {switchMenu.comunidad && (
-          <div className="absolute top-14  -left-5 ">
-            <MenuInterno
-              opciones={[
-                <Link key="opcion1" href="/voluntarios">
-                  Voluntarios
-                </Link>,
-                <Link key="opcion2" href="/foro">
-                  Foro
-                </Link>,
-              ]}
-              tamaño="lg:w-[180%] md:w-[150%] md:py-4 md:rounded-br-[10px] md:rounded-bl-[10px]"
-            />
-          </div>
-        )}
+        <div
+          className={`absolute top-14 -left-5 transition-all duration-300 -z-20 ${
+            switchMenu.comunidad
+              ? "translate-y-0 opacity-100"
+              : "-translate-y-12 opacity-0"
+          }`}
+        >
+          <MenuInterno
+            opciones={[
+              <Link key="opcion1" href="/voluntarios">
+                Voluntarios
+              </Link>,
+              <Link key="opcion2" href="/foro">
+                Foro
+              </Link>,
+            ]}
+            tamaño="lg:w-[180%] md:w-[150%] md:py-4 md:rounded-br-[10px] md:rounded-bl-[10px]"
+          />
+        </div>
       </li>
       <li
         className="flex gap-1 items-center cursor-pointer relative "
         data-menu="servicios"
       >
         <span
-          className="flex items-center gap-2"
+          className="flex items-center gap-2 text-white hover:text-gray-300 focus:outline-none"
           onClick={(e) => handlerMenu(e, "servicio")}
         >
           Servicios
@@ -105,32 +113,34 @@ export default function MenuDesktop() {
               switchMenu.servicio ? "rotate-180" : " rotate-0"
             } duration-200`}
           />
-          {switchMenu.servicio && (
-            <div className="absolute top-14 -left-8 ">
-              {switchMenu.servicio && (
-                <MenuInterno
-                  opciones={[
-                    <Link key="opcion1" href="/contactanos">
-                      Contacto
-                    </Link>,
-                    <Link key="opcion2" href="/*">
-                      Como Denunciar
-                    </Link>,
-                    <Link key="opcion3" href="/*">
-                      Atención Cliente
-                    </Link>,
-                    <Link key="opcion4" href="/*">
-                      Galeria Solidaria
-                    </Link>,
-                    <Link key="opcion5" href="/*">
-                      Cursos
-                    </Link>,
-                  ]}
-                  tamaño="md:w-[130%] md:py-4 md:rounded-br-[10px] md:rounded-bl-[10px]"
-                />
-              )}
-            </div>
-          )}
+          <div
+            className={`absolute top-14 -left-8 transition-all duration-300 -z-20 ${
+              switchMenu.servicio
+                ? "translate-y-0 opacity-100"
+                : "-translate-y-12 opacity-0"
+            }`}
+          >
+            <MenuInterno
+              opciones={[
+                <Link key="opcion1" href="/contactanos">
+                  Contacto
+                </Link>,
+                <Link key="opcion2" href="/*">
+                  Como Denunciar
+                </Link>,
+                <Link key="opcion3" href="/*">
+                  Atención Cliente
+                </Link>,
+                <Link key="opcion4" href="/*">
+                  Galeria Solidaria
+                </Link>,
+                <Link key="opcion5" href="/*">
+                  Cursos
+                </Link>,
+              ]}
+              tamaño="md:w-[130%] md:py-4 md:rounded-br-[10px] md:rounded-bl-[10px]"
+            />
+          </div>
         </span>
       </li>
     </ul>
