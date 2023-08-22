7d47eb3d6a1ac1768a555c08f46108c495e664fa
Felipe Moriones <96366130+Moriones20@users.noreply.github.com>
2023-08-22 15:57:27 -0500

    Agregar propiedades y ajustar line-clamp y seccion image-title

--- src/components/Shop/ShoppingCards.jsx
+++ src/components/Shop/ShoppingCards.jsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
 import ContactUsButton from "./ContactUsbutton";
 
-export default function ShoppingCards({ title, image, detail, redirect }) {
-    return (
-        <div className="   border-[0.1px] border-[#80808000] border-b-0 shadow-2xl m-auto flex flex-col  justify-between
+export default function ShoppingCards({
+  key,
+  title,
+  image,
+  description,
+  redirect,
+}) {
+  return (
+    <div
+      key={key}
+      className="border-[0.1px] border-[#80808000] border-b-0 shadow-2xl m-auto flex flex-col justify-between 
         w-[13rem]
         min-[350px]:w-[11rem]
         min-[400px]:w-[12rem]
@@ -11,19 +20,33 @@ export default function ShoppingCards({ title, image, detail, redirect }) {
         sm:w-[14rem]
         md:w-[14rem]
         lg:w-[18rem]
-        xl:w-[19rem] min-h-[24rem]  ">
+        xl:w-[19rem]
+        sm:min-h-[24rem]
+        min-h-[28rem]"
+    >
+      <div>
+        <section className="h-[8rem] w-full bg-blue-400 ">
+          {/* <Image
+          src={image}
+          alt={title}
+          width={50}
+          height={50}
+          layout="fixed"
+          className=""
+        /> */}
+        </section>
 
-            <section className="h-[8rem] w-full bg-blue-400 ">
+        <h5 className=" text-base text-center md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl line-clamp-1 font-semibold">
+          {title}
+        </h5>
+      </div>
+      <article className="w-[95%] p-2 text-left">
+        <p className="break-words p-1 text-sm min-[350px]:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl line-clamp-6 sm:line-clamp-4">
+          {description}
+        </p>
+      </article>
 
-            </section>
-
-            <h5 className=" text-base text-center md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">{title}</h5>
-            <article className="w-[95%] p-2 text-left ">
-                <p className=" break-words p-1 text-sm min-[350px]:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl">{detail}</p>
-            </article>
-
-            <ContactUsButton />
-
-        </div>
-    )
+      <ContactUsButton />
+    </div>
+  );
 }
