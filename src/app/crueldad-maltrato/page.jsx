6e24e1a4beeb90099ec5002de4e8fae4dcb486e9
gonlla8777 <utngonzalollanos@gmail.com>
2023-08-24 15:47:25 -0300

    pull develop

--- src/app/crueldad-maltrato/page.jsx
+++ src/app/crueldad-maltrato/page.jsx
@@ -7,18 +7,18 @@ import image1 from "../../../public/images/crueldad-maltrato.png";
 export default function page() {
   return (
     <div className=" min-h-screen flex flex-col mt-[70px] pb-[5rem] items-center gap-12 ">
-      <section className="w-full  h-fit md:h-[14rem] relative bg-black ">
-        <article>
+      <section className="w-full h-[14rem] md:h-[14rem] relative">
+        <article className="max-w-screen-xl mx-auto">
           <Image
             src={image1}
             alt="background"
             fill
-            className="hidden md:block"
+            className="hidden md:block object-cover w-full h-full"
           />
           <Image
             src={image1}
             alt="background"
-            className="w-full h-[10rem]  md:hidden"
+            className="w-full h-[14rem] md:hidden object-cover absolute"
           />
         </article>
       </section>
