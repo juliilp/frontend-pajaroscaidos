e80b06dd2241fe5c5be0fddbf8e2e9bb766411bf
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-08-31 20:09:29 -0300

    menos divs

--- src/app/page.js
+++ src/app/page.js
@@ -30,12 +30,10 @@ export default function Home() {
     <section className="mt-[70px]">
       <MainHome banner={images} />
 
-      <div className="w-full flex flex-col gap-6 lg:flex-row lg:gap-0">
+      <div className="w-full flex flex-col gap-6 xl:flex-row lg:gap-0 xl:px-8">
         <Campañas />
 
-        <div className="lg:w-[30%] lg:ml-6">
-          <NuestraComunidad />
-        </div>
+        <NuestraComunidad />
       </div>
     </section>
   );
