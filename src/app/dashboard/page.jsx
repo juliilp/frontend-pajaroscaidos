1f9f0ac1cec8d675202ba58486e1a4a648c73c26
MiguelZ01 <jesusmigu3lzr2004@gmail.com>
2023-09-17 01:16:24 -0500

    Sidebar

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Sidebar from "@/components/Sidebar/Sidebar";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+}
