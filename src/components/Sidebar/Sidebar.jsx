ed6a231f1fd1890273f86dff5bf0599e4e3f0a1b
Felipe Moriones <96366130+Moriones20@users.noreply.github.com>
2023-09-19 14:17:06 -0500

    Deleted publicidad

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -89,26 +89,6 @@ function Sidebar() {
             </li>
             <span>Usuarios</span>
           </Link>
-          <Link
-            href="/dashboard/publicidad"
-            className="flex items-center hover:bg-[#444444] py-2"
-          >
-            <li>
-              <svg
-                width="50px"
-                height="50px"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M12 9.5C13.3807 9.5 14.5 10.6193 14.5 12C14.5 13.3807 13.3807 14.5 12 14.5C10.6193 14.5 9.5 13.3807 9.5 12C9.5 10.6193 10.6193 9.5 12 9.5Z"
-                  fill="#ffffff"
-                />
-              </svg>
-            </li>
-            <span>Publicidad</span>
-          </Link>
         </ul>
       </div>
     </div>
