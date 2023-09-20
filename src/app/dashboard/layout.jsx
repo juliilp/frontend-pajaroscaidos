5102f8f86195f32c2c189874587007ab9c2acfdf
Felipe Moriones <96366130+Moriones20@users.noreply.github.com>
2023-09-19 19:15:09 -0500

    Added loading, deleted usuarios.json and fixed details

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -2,7 +2,7 @@ import Sidebar from "@/components/Sidebar/Sidebar";
 
 export default function DashboardLayout({ children }) {
   return (
-    <main className="flex mt-[70px]">
+    <main className="flex min-h-screen">
       <Sidebar />
       <section className="w-[80%]">{children}</section>
     </main>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 function Sidebar() {
   return (
-    <div className="bg-[#4F4F4F] h-auto w-[20%] text-white">
+    <div className="bg-[#4F4F4F] h-auto w-[20%] pt-[70px] text-white">
       <h1 className="font-bold text-2xl border-b-4 border-b-[#444444] text-center p-4 ">
         Panel administrador
       </h1>
