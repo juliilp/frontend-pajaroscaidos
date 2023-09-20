c893f300eb432078dae2f4290458211c61e4b928
Felipe Moriones <96366130+Moriones20@users.noreply.github.com>
2023-09-20 15:53:54 -0500

    Changed div to aside

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,12 +3,12 @@ import CustomLink from "./CustomLink";
 
 function Sidebar() {
   return (
-    <div className="bg-[#4F4F4F] h-auto w-[20%] pt-[70px] text-white">
+    <aside className="bg-[#4F4F4F] h-auto w-[20%] pt-[70px] text-white">
       <h1 className="font-bold text-2xl border-b-4 border-b-[#444444] text-center p-4 ">
         Panel administrador
       </h1>
       <CustomLink />
-    </div>
+    </aside>
   );
 }
 
