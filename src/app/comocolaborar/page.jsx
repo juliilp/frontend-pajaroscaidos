53a5e3dba7fa52f473dfd292ba681b5e8c5c9355
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-09-25 16:42:52 -0300

    redirecciones

--- src/app/comocolaborar/page.jsx
+++ src/app/comocolaborar/page.jsx
@@ -7,7 +7,7 @@ import bgimage from "@/../public/images/Como Colaborar/ComoColaborar.png";
 
 export default function page() {
   return (
-    <section className="mt-[70px]">
+    <section className="mt-[70px] pb-48">
       <section className="w-full relative min-[320px]:h-[9rem] min-[400px]:h-[10rem] min-[500px]:h-[12rem] sm:h-[13rem]  md:h-[14rem] lg:h-[15rem] 2xl:h-[18rem]">
         <Image src={bgimage} alt="bg-tucan" fill className="w-full " />
       </section>
@@ -25,25 +25,27 @@ export default function page() {
           cosas.
         </p>
       </div>
-      <div className="flex items-center justify-center w-full  px-[5%] gap-10 flex-wrap py-2 ">
+      <article className="flex items-center justify-center w-full  px-[5%] gap-10 flex-wrap py-2">
+        <CardComoColaborar
+          titulo="Voluntarios"
+          parrafo="Podes colaborar siendo voluntario de las multiples tareas que lleva adelante Pájaros Caídos. Tu com`promiso es esencial para seguir creciendo."
+          nombreBoton="VOLUNTARIADOS"
+          image={Voluntarios}
+          enlace="https://mail.google.com/mail/?view=cm&to=pajaroscaidos@yahoo.com.ar"
+        />
         <CardComoColaborar
           titulo="Materiales de construcción"
           parrafo="Necesitamos materiales de construcción para ampliar y mejorar nuestros Refugios. Necesitamos chapas, tirantes, hierros, malla de alambre, pintura entre otras cosas."
           image={Construccion}
         />
         <CardComoColaborar
-          titulo="Voluntarios"
-          parrafo="Podes colaborar siendo voluntario de las multiples tareas que lleva adelante Pájaros Caídos. Tu com`promiso es esencial para seguir creciendo."
-          nombreBoton="VOLUNTARIADOS"
-          image={Voluntarios}
-        />
-        <CardComoColaborar
           titulo="Donaciones"
           parrafo="Para poder seguir trabajando por el bienestar de las aves y crecer, necesitamos de tu colaboracion. Desde acá podrás ayudarnos a construir nuevos refugios para aves, dictar cursos de capacitación, realizar campañas de concientización, formular denuncias por actos de maltrato o crueldad, trabajar contra el tráfico ilegal de animales, entre otras tantas cosas."
           nombreBoton="MERCADO PAGO"
           image={Donaciones}
+          enlace="https://link.mercadopago.com.ar/pajaroscaidos"
         />
-      </div>
+      </article>
     </section>
   );
 }
