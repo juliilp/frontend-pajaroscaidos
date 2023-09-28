e35f61bf46faec83a329e9cd5e9dbd68f43b8602
Felipe Moriones <moriones06082001@gmail.com>
2023-09-28 15:44:50 -0500

    Fetch posts and orderPosts

--- src/app/perfil/page.jsx
+++ src/app/perfil/page.jsx
@@ -1,38 +1,24 @@
 "use client";
 import { useEffect, useState } from "react";
 import { CustomContext } from "@/store/ContextProvider";
-import { useRouter } from "next/navigation";
-import api from "../../api/api";
 import Loading from "../loading";
 import InfoProfile from "@/components/Perfil/InfoProfile";
 import AboutMe from "@/components/Perfil/AboutMe";
 import MyPosts from "@/components/Perfil/MyPosts";
 
 export default function Perfil() {
-  const router = useRouter();
   const { UserContext } = CustomContext();
   const [user, setUser] = useState();
-  const [publications, setPublications] = useState([]);
 
   useEffect(() => {
     setUser(UserContext);
-
-    // async function getUser() {
-    //   const response = await api.get(`/user/${user.id}`);
-    //   setUserContext(response.data.user);
-    // }
-    // Cuanto se cierre la modal de actualizar perfil, se ejecuta la funcion anterior.
-
-    async function userPublications() {
-      const result = await api(`/user/${user.id}/?filter=publications`);
-      const data = result.data.user.publications;
-      return setPublications(data);
+    /* async function getUser() {
+       const response = await api.get(`/user/${user.id}`);
+       setUserContext(response.data.user);
     }
-
-    if (user) {
-      userPublications();
-    }
-  }, [UserContext, user, router]);
+    Cuanto se cierre la modal de actualizar perfil, se ejecuta la funcion anterior.
+    TRABAJANDO EN ESTO NO BORRAR! */
+  }, [UserContext, user]);
 
   if (!user) {
     return <Loading />;
@@ -42,7 +28,7 @@ export default function Perfil() {
     <article className="flex flex-col mt-[70px] items-center">
       <InfoProfile user={user} />
       <AboutMe description={user.description} />
-      <MyPosts />
+      <MyPosts user={user} />
     </article>
   );
 }
--- src/components/Perfil/MyPosts.jsx
+++ src/components/Perfil/MyPosts.jsx
@@ -1,9 +1,66 @@
-export default function MyPosts() {
+import React, { useEffect, useState } from "react";
+import api from "../../api/api";
+import sortPostsByDate from "@/helpers/orderPosts";
+import CardForo from "../CardForo";
+
+export default function MyPosts({ user }) {
+  const [posts, setPosts] = useState({ posts: [], order: "recent" });
+
+  useEffect(() => {
+    async function userPosts() {
+      const result = await api(`/user/${user.id}/?filter=publications`);
+      const data = result.data.user.publications;
+      setPosts((prevPosts) => ({
+        ...prevPosts,
+        posts: sortPostsByDate(data, posts.order),
+      }));
+    }
+
+    if (user) {
+      userPosts();
+    }
+  }, [posts.order, user]);
+
+  const handleOrderChange = (event) => {
+    const newOrder = event.target.value;
+
+    setPosts((prevPosts) => ({
+      ...prevPosts,
+      order: newOrder,
+      posts: sortPostsByDate(prevPosts.posts, newOrder),
+    }));
+  };
+
   return (
     <section className="flex p-6 flex-col justify-center gap-4 items-center mt-3 lg:w-[840px] rounded-xl border-[#C4C4C4] border-2 shadow-md">
-      <h3 className="text-left w-full text-xl font-semibold">
-        Mis publicaciones
-      </h3>
+      <div className="flex justify-between w-full">
+        <h3 className="text-left w-full text-2xl font-semibold">
+          Mis publicaciones
+        </h3>
+        <select onChange={handleOrderChange}>
+          <option value="recent">Recientes</option>
+          <option value="old">Antiguos</option>
+        </select>
+      </div>
+      {posts.posts && posts.posts.length > 0 ? (
+        posts.posts.map((post) => {
+          return (
+            <CardForo
+              key={post.id}
+              titulo={post.title}
+              tiempo={post.updatedAt}
+              usuario={user.nick_name}
+              like={post.reactions.length}
+              message={post.comments.length}
+              image={post.image[0]}
+              id={post.id}
+              reactions={post.reactions}
+            />
+          );
+        })
+      ) : (
+        <p>No hay publicaciones.</p>
+      )}
     </section>
   );
 }
