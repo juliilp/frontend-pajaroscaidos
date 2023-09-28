732d7bac23a6c2518cd556a3704c7a964f0a8a62
Felipe Moriones <moriones06082001@gmail.com>
2023-09-28 15:43:52 -0500

    Changed xl to 2xl

--- src/components/Perfil/AboutMe.jsx
+++ src/components/Perfil/AboutMe.jsx
@@ -4,7 +4,7 @@ export default function AboutMe({ description }) {
   return (
     <section className="flex p-6 flex-col justify-center gap-4 items-center mt-3 lg:w-[840px] rounded-xl border-[#C4C4C4] border-2 shadow-md">
       <div className="flex justify-between w-full">
-        <h3 className="text-xl font-semibold">Sobre mi</h3>
+        <h3 className="text-2xl font-semibold">Sobre mi</h3>
         <BiEditAlt size={30} className="cursor-pointer" />
       </div>
       <p className="w-full">{description || "No hay descripcion"}</p>
