54bce57cb331c7ed2292a34c9227ad94ca42c80f
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-10-02 14:03:46 -0300

    loading mobile

--- src/components/CardVoluntario/CardVoluntarioLoading.jsx
+++ src/components/CardVoluntario/CardVoluntarioLoading.jsx
@@ -17,7 +17,7 @@ export default function CardVoluntarioLoading() {
       </article>
 
       <article className="flex p-4 border rounded-lg shadow-md md:hidden">
-        <div className="w-1/3 pr-4">
+        <article className="w-1/3 pr-4 relative">
           <Image
             src={LoadingImagen}
             alt="Imagen"
@@ -25,11 +25,12 @@ export default function CardVoluntarioLoading() {
             height={500}
             className="w-full h-auto rounded-lg"
           />
-        </div>
-        <div className="w-2/3">
-          <h2 className="text-2xl font-bold mb-2">Soy titulo</h2>
-          <p className="text-gray-700">Soy texto</p>
-        </div>
+          <Spinner />
+        </article>
+        <article className="w-2/3">
+          <div className="h-[24px] w-[104px] animate-pulse bg-neutral-400 rounded-full mb-2" />
+          <div className="w-[64px] animate-pulse h-[15px] bg-neutral-400 rounded-full" />
+        </article>
       </article>
     </section>
   );
