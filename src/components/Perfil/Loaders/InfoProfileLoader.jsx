cdd05ee9f3a68d6b72eecff1d2035c7bdff896b0
Felipe Moriones <moriones06082001@gmail.com>
2023-10-04 13:23:29 -0500

    Add (Profile): Loaders

---
+++ src/components/Perfil/Loaders/InfoProfileLoader.jsx
@@ -0,0 +1,19 @@
+export default function InfoProfileLoader() {
+  return (
+    <section className="flex p-6 flex-col justify-center gap-4 items-center mt-3 lg:w-[840px] rounded-xl border-[#C4C4C4] border-2 shadow-md animate-pulse">
+      <header className="flex h-28 w-full bg-neutral-400 rounded-lg"></header>
+      <section className="flex flex-col items-start w-full py-1 gap-3">
+        <div className="w-full flex flex-col gap-2">
+          <div className="bg-neutral-400 h-6 w-[35%] rounded"></div>
+          <div className="bg-neutral-400 h-6 w-[30%] rounded"></div>
+          <div className="bg-neutral-400 h-6 w-[40%] rounded"></div>
+          <div className="bg-neutral-400 h-6 w-[35%] rounded"></div>
+        </div>
+        <div className="w-full flex flex-col gap-2">
+          <div className="bg-neutral-400 h-6 w-[50%] rounded"></div>
+          <div className="bg-neutral-400 h-6 w-[40%] rounded"></div>
+        </div>
+      </section>
+    </section>
+  );
+}
