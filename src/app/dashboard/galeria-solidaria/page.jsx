85be16d51919141b00c97e84cbfe19d99417f9af
Felipe Moriones <moriones06082001@gmail.com>
2023-10-04 19:44:06 -0500

    Fix (dashboard/galeria-solidaria): Changed to React function component and formatted code

--- src/app/dashboard/galeria-solidaria/page.jsx
+++ src/app/dashboard/galeria-solidaria/page.jsx
@@ -1,87 +1,110 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import ItemModal from "@/components/Dashboard/OpcionesInicio/galeria-solidaria/ItemModal";
-import { getItemsShop } from '@/api/apiCall/functions';
+import { getItemsShop } from "@/api/apiCall/functions";
 import ProductSection from "@/components/Dashboard/OpcionesInicio/galeria-solidaria/ProductSection";
 
-function page() {
+export default function Page() {
   const [data, setData] = useState({});
   const [visibleModal, setVisibleModal] = useState(false);
-  const [modalType, setModalType] = useState('')
-  const [itemToEdit, setItemToEdit] = useState({})
-  const [actualPage,setActualPage]=useState(1)
-  const [totalPages,setTotalPages]=useState(0)
+  const [modalType, setModalType] = useState("");
+  const [itemToEdit, setItemToEdit] = useState({});
+  const [actualPage, setActualPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
   useEffect(() => {
     const action = async () => {
       try {
         const data = await getItemsShop(actualPage);
-        setData(data.items)
-        setTotalPages(data.items.totalPages)
-        console.log(data.items.totalPages,'total apg');
+        setData(data.items);
+        setTotalPages(data.items.totalPages);
+        console.log(data.items.totalPages, "total apg");
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    }
+    };
     action();
-  }, [actualPage])
+  }, [actualPage]);
+
   const openCreateModal = () => {
-    setItemToEdit(null)
-    setModalType('create');
-    setVisibleModal(true)
-  }
+    setItemToEdit(null);
+    setModalType("create");
+    setVisibleModal(true);
+  };
+
   const openEditModal = (event, data) => {
-    event.preventDefault()
+    event.preventDefault();
 
     const itemprops = {
-      id:data.id,
+      id: data.id,
       image: data.image[0].secure_url,
       title: data.title,
       description: data.description,
       categories: [data.categories[0]?.name],
-      category: [data.categories[0]?.name]
-    }
-    setItemToEdit(itemprops)
-    setModalType('edit');
-    setVisibleModal(true)
-  }
+      category: [data.categories[0]?.name],
+    };
+    setItemToEdit(itemprops);
+    setModalType("edit");
+    setVisibleModal(true);
+  };
+
   const closeModal = () => {
-    setVisibleModal(false)
-  }
-  const changePage=(event)=>{
+    setVisibleModal(false);
+  };
+
+  const changePage = (event) => {
     event.preventDefault();
-    const {value}=event.target;
-    if(value<1) return alert('No hay mas paginas');
-    if (value>totalPages) return alert ('No se puede avanzar');
-    setActualPage(value)
-  }
-  const pagees=()=>{
-    let array=[];
+    const { value } = event.target;
+    if (value < 1) return alert("No hay mas paginas");
+    if (value > totalPages) return alert("No se puede avanzar");
+    setActualPage(value);
+  };
+
+  const pagees = () => {
+    let array = [];
     console.log(totalPages);
-    if(data?.totalPages){
+    if (data?.totalPages) {
       for (let i = 0; i < totalPages; i++) {
-        array.push(parseInt(i+1))
+        array.push(parseInt(i + 1));
       }
     }
-    
-    return array
-  }
+
+    return array;
+  };
+
   return (
-    <div className=" text-sm md:text-sm  lg:text-base xl:text-lg 2xl:text-xl 
-     min-h-screen w-full flex flex-col justify-center items-center gap-6 pt-[90px] pb-10">
-      {visibleModal && <ItemModal closeModal={closeModal} ModalType={modalType} itemToEdit={itemToEdit} />}
+    <div
+      className=" text-sm md:text-sm  lg:text-base xl:text-lg 2xl:text-xl 
+     min-h-screen w-full flex flex-col justify-center items-center gap-6 pt-[90px] pb-10"
+    >
+      {visibleModal && (
+        <ItemModal
+          closeModal={closeModal}
+          ModalType={modalType}
+          itemToEdit={itemToEdit}
+        />
+      )}
       <section>
-        <h1 className="text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl">Galeria Solidaria</h1>
+        <h1 className="text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl">
+          Galeria Solidaria
+        </h1>
       </section>
-      
+
       <section className="  bg-[#4F4F4F] w-[95%] min-h-[20rem] flex flex-col gap-4   p-6 pb-0 pt-12 rounded-lg">
-        <ProductSection data={data} openCreateModal={openCreateModal} openEditModal={openEditModal}/>
+        <ProductSection
+          data={data}
+          openCreateModal={openCreateModal}
+          openEditModal={openEditModal}
+        />
       </section>
       <section className="flex  justify-center gap-2 ">
-      {data?.totalPages&& pagees().map((i,key)=>
-        <button key={key} value={i} onClick={changePage}>{i}</button>
-        )}
+        {data?.totalPages &&
+          pagees().map((i, key) => (
+            <button key={key} value={i} onClick={changePage}>
+              {i}
+            </button>
+          ))}
       </section>
     </div>
   );
 }
-export default page;
