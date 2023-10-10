da483ed616891b6f0ac2a24102d69d94602317ba
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-10-09 23:25:31 -0300

    espaciado y codigo no repetido

--- src/app/acciones/articulo2/page.jsx
+++ src/app/acciones/articulo2/page.jsx
@@ -1,33 +1,23 @@
-import MainSectionArticulo2 from "@/components/Acciones/Articulo2/MainSectionArticulo2";
-import Section2Articulo2 from "@/components/Acciones/Articulo2/Section2Articulo2";
-import Section3Articulo2 from "@/components/Acciones/Articulo2/Section3Articulo2";
-import Section4Articulo2 from "@/components/Acciones/Articulo2/Section4Articulo2";
 import Image from "next/image";
 import bgimage from "../../../../public/images/Feria Villa Dominico/villa-dominico-BG.png";
-import image1 from "../../../../public/images/Feria Villa Dominico/villa-dominico-1.png";
-import image2 from "../../../../public/images/Feria Villa Dominico/villa-dominico-2.png";
-import image3 from "../../../../public/images/Feria Villa Dominico/villa-dominico-3.png";
-import image4 from "../../../../public/images/Feria Villa Dominico/villa-dominico-4.png";
-import image5 from "../../../../public/images/Feria Villa Dominico/villa-dominico-5.png";
+import ArraySection from "@/components/Acciones/Articulo2/ArraySection";
 
 export default function page() {
   return (
-    <div className=" font-semibold min-h-screen flex flex-col items-center pt-[70px] gap-12 pb-12">
-      <section className="w-full relative h-[8rem] min-[320px]:h-[9rem] min-[400px]:h-[10rem] min-[500px]:h-[12rem] sm:h-[13rem]  md:h-[14rem] lg:h-[15rem] 2xl:h-[17rem]">
+    <section className=" font-semibold min-h-screen flex flex-col items-center pt-[70px] pb-12">
+      <article className="w-full relative h-[8rem] min-[320px]:h-[9rem] min-[400px]:h-[10rem] min-[500px]:h-[12rem] sm:h-[13rem]  md:h-[14rem] lg:h-[15rem] 2xl:h-[17rem]">
         <Image src={bgimage} alt="bg-tucan" fill priority />
-      </section>
-      <main className="flex flex-col items-center w-11/12 gap-12">
-        <MainSectionArticulo2 image1={image1} image2={image2} />
-      </main>
-      <section className="flex flex-col w-11/12 items-center gap-12">
-        <Section2Articulo2 image={image3} />
-      </section>
-      <section className="flex flex-col w-11/12 items-center gap-12">
-        <Section3Articulo2 image={image4} />
-      </section>
-      <section className="flex flex-col w-11/12 items-center gap-12">
-        <Section4Articulo2 image={image5} />
-      </section>
-    </div>
+      </article>
+      {ArraySection.map(({ componente }, key) => {
+        return (
+          <article
+            key={key}
+            className="flex flex-col items-center w-11/12 gap-6 mt-6"
+          >
+            {componente}
+          </article>
+        );
+      })}
+    </section>
   );
 }
