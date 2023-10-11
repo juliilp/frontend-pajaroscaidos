ce36e5ef82ea210b809ca7b6c1515602ff64009e
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-10-11 01:47:30 -0300

    Distancia entre items

--- src/app/nosotros/page.jsx
+++ src/app/nosotros/page.jsx
@@ -8,16 +8,16 @@ import MainAboutSection from "@/components/Nosotros/MainAboutSection";
 
 export default function page() {
   return (
-    <div className="font-semibold min-h-screen flex flex-col items-center gap-12 lg:text-lg xl:text-xl pt-[70px] 2xl:text-2xl">
+    <div className=" min-h-screen flex flex-col items-center gap-6 lg:text-lg xl:text-xl pt-[70px] 2xl:text-2xl">
       <section className="h-[18rem] 2xl:h-[23rem] bg-black relative w-full">
         <Image src={image1} alt="Image1" fill />
       </section>
 
-      <main className="flex items-center flex-col w-full text-center gap-12">
+      <main className="flex items-center flex-col w-11/12 text-center gap-6">
         <MainAboutSection />
       </main>
 
-      <section className="flex flex-col items-center w-11/12 gap-12">
+      <section className="flex flex-col items-center w-11/12 gap-6">
         <AboutSection2 />
       </section>
 
