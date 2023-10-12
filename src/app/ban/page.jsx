48b9e2bc9a8ba7305eb9fc7d542922b32a869286
Felipe Moriones <moriones06082001@gmail.com>
2023-10-12 14:57:35 -0500

    Add (ban): Design was added to the banned page and link to the email to ask why they banned it

--- src/app/ban/page.jsx
+++ src/app/ban/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect } from "react";
 import { signOut, useSession } from "next-auth/react";
+import { BiSolidUserX } from "react-icons/bi";
 
 export default function Ban() {
   const { data: session } = useSession();
@@ -12,11 +13,26 @@ export default function Ban() {
   }, [session]);
 
   return (
-    <div className="flex items-center justify-center w-screen h-screen">
-      <h1>
-        Te baneamos la cuenta capo! Ponete en contacto con administración para resolver el problema
-        xd
-      </h1>
-    </div>
+    <section className="flex justify-center items-center w-full h-screen">
+      <div className="bg-[#3D3D3D] flex flex-col justify-center items-center gap-7 text-white rounded-lg px-16 py-8">
+        <div className="flex gap-3 items-end">
+          <BiSolidUserX size={40} color="#D22929" />
+          <h1 className="font-semibold text-3xl">
+            Oops! Tu cuenta ha sido baneada
+          </h1>
+        </div>
+        <p className="text-base">
+          Ponte en contacto con administración para tener mas información.
+        </p>
+        <button className="bg-[#60EA4A] px-4 py-2 font-semibold rounded-md text-black">
+          <a
+            href="mailto:pajaroscaidos@yahoo.com.ar?subject=Usuario%20baneado"
+            target="_blank"
+          >
+            Contacto
+          </a>
+        </button>
+      </div>
+    </section>
   );
 }
