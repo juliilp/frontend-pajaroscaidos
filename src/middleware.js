6716d97e852d1ef623a3dda83f0221530a54c447
unknown <joakig6@gmail.com>
2023-10-13 13:39:30 -0300

    dashboard pages now is protected

--- src/middleware.js
+++ src/middleware.js
@@ -7,27 +7,31 @@ export async function middleware(req) {
   const privateRoutes = ["/perfil"];
   const otherRoutes = ["/login", "/registro"];
   const resquestedPage = req.nextUrl.pathname;
+  const inAdminPages = resquestedPage.includes("/dashboard");
+  const value = JSON.parse(sesion?.value);
+  const isAdmin = value?.isAdmin;
+  const isPrincipalAdmin = value?.isPrincipalAdmin;
+  const url = req.nextUrl.clone();
+  url.pathname = "/";
+  if (existCookie&&inAdminPages && !isAdmin) {///validacion admins
+    return NextResponse.redirect(url);
+  }
   if (
     privateRoutes.includes(resquestedPage) &&
     !isActiveSesion &&
     existCookie
   ) {
-    const url = req.nextUrl.clone();
-    url.pathname = "/";
     return NextResponse.redirect(url);
   } else if (
     existCookie &&
     isActiveSesion &&
     otherRoutes.includes(resquestedPage)
   ) {
-    const url = req.nextUrl.clone();
-    url.pathname = "/";
-
     return NextResponse.redirect(url);
   }
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/login", "/registro", "/perfil"],
+  matcher: ["/login", "/registro", "/perfil", '/dashboard',"/dashboard/((?!general).*)"],
 };
