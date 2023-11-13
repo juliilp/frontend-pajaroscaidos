a5c5104af249ef11fa9c0326b952ba7e465ff01c
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-11-13 19:21:26 -0300

    CardRCE

---
+++ src/components/EmpresasParticipantesRCE/dataEmpresas.js
@@ -0,0 +1,30 @@
+import ImagenPrueba from "@/../public/images/cursos/card1.webp";
+
+const dataEmpresas = [
+  {
+    titulo: "Farmacity",
+    imagen: ImagenPrueba,
+    texto:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate minima aliquid autem, ducimus deserunt ab consequatur accusamus doloribus est quia!",
+  },
+  {
+    titulo: "SoyHenry",
+    imagen: ImagenPrueba,
+    texto:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate minima aliquid autem, ducimus deserunt ab consequatur accusamus doloribus est quia!Lorem ipsum dolor sit amet consectetur adipisicing elit!",
+  },
+  {
+    titulo: "Empresa",
+    imagen: ImagenPrueba,
+    texto:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate minima aliquid autem, ducimus deserunt ab consequatur accusamus doloribus est quia!Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate minima aliquid autem, ducimus deserunt ab consequatur accusamus doloribus est quia!",
+  },
+  {
+    titulo: "A definir",
+    imagen: ImagenPrueba,
+    texto:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate minima aliquid autem, ducimus deserunt ab consequatur accusamus doloribus est quia!Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate minima aliquid autem, ducimus deserunt ab consequatur accusamus doloribus est quia!Lorem ipsum dolor sit!.Cupiditate minima aliquid autem, ducimus deserunt ab consequatur accusamus ",
+  },
+];
+
+export default dataEmpresas;
