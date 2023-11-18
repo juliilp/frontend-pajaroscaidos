a156c8d7dff44858810e9219d009e62be97ba002
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-11-18 19:35:50 -0300

    Cambio url back-end de maxi a nico

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: 'https://pajaros-caidos-backend.onrender.com', // BACK RENDER
-  baseURL: "https://pajaros-caidos-back-end2-0-git-main-maxmdr2022.vercel.app", // BACK VERCEL
+  // baseURL: "https://pajaros-caidos-backend.onrender.com", // BACK RENDER
+  // baseURL: "https://pajaros-caidos-back-end2-0.vercel.app/", // BACK VERCEL maxi
+  baseURL: "https://pajaros-caidos-back-end2-0-livid.vercel.app", //BACK VERCEL NICO
   // baseURL: "http://localhost:3001", // BACK local
   withCredentials: true,
 });
