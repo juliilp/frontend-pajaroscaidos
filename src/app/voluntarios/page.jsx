d7c118ed85d7d1b830b2ab436ca827b1da918228
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-11-20 19:53:07 -0300

    Boton y redireccion

--- src/app/voluntarios/page.jsx
+++ src/app/voluntarios/page.jsx
@@ -6,6 +6,7 @@ import banner from "../../../public/images/Voluntarios/voluntarios1.webp";
 import CardVoluntario from "@/components/CardVoluntario/CardVoluntario";
 import CardVoluntarioLoading from "@/components/CardVoluntario/CardVoluntarioLoading";
 import Modal from "@/components/CardVoluntario/Modal";
+import Link from "next/link";
 
 export default function Voluntarios() {
   const [users, setUsers] = useState([]);
@@ -42,7 +43,12 @@ export default function Voluntarios() {
       <section className="w-full md:px-[10%]">
         <Image src={banner} alt="banner" width={2000} height={600} priority />
       </section>
-
+      <Link
+        href="/quieroservoluntario"
+        className="text-sm min-[350px]:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl text-white bg-green p-2 md:p-3 hover:text-gray-100 hover:bg-[#13b113] duration-200"
+      >
+        Quiero ser voluntario
+      </Link>
       <article className="flex flex-col md:grid grid-cols-2 gap-12 lg:grid-cols-3 lg:gap-20 xl:grid-cols-4">
         {users && users.length > 0
           ? users.map(({ description, first_name, avatar }, key) => {
