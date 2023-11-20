5333a8ea2875534bcbda7805b2af78680342ca9a
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-11-20 19:38:08 -0300

    semantica html

--- src/components/CardVoluntario/CardVoluntario.jsx
+++ src/components/CardVoluntario/CardVoluntario.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 export default function CardVoluntario({ imagen, titulo, onClick }) {
   return (
-    <div className="hidden md:block ">
-      <div
+    <section className="hidden md:block ">
+      <article
         className="w-44 h-44 border border-gray-300 p-4 transition-transform hover:scale-105 relative rounded-lg cursor-pointer"
         onClick={onClick}
       >
@@ -15,10 +15,10 @@ export default function CardVoluntario({ imagen, titulo, onClick }) {
           height={500}
           className="w-full h-full object-cover absolute inset-0 rounded-lg"
         />
-        <div className="absolute inset-0 flex flex-col justify-center items-center text-center bg-black bg-opacity-70 opacity-0 transition-opacity hover:opacity-100 border rounded-lg ">
+        <article className="absolute inset-0 flex flex-col justify-center items-center text-center bg-black bg-opacity-70 opacity-0 transition-opacity hover:opacity-100 border rounded-lg ">
           <h3 className="text-2xl font-bold text-white">{titulo}</h3>
-        </div>
-      </div>
-    </div>
+        </article>
+      </article>
+    </section>
   );
 }
