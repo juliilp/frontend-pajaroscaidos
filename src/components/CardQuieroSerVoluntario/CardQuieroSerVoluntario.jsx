9b23801cb9e1b568267f42e821b107b9e08665bb
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-11-22 18:57:32 -0300

    diseño basico

--- src/components/CardQuieroSerVoluntario/CardQuieroSerVoluntario.jsx
+++ src/components/CardQuieroSerVoluntario/CardQuieroSerVoluntario.jsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function CardQuieroSerVoluntario({ titulo, imagen }) {
   return (
-    <section>
-      <h2>{titulo}</h2>
+    <section className="w-full max-w-[320px] h-[200px] bg-[#c2c2c2] rounded-md p-6">
+      <h2 className="text-[#0C6410] text-xl font-semibold text-center">
+        {titulo}
+      </h2>
       <Image src={imagen} alt="imagen" />
     </section>
   );
