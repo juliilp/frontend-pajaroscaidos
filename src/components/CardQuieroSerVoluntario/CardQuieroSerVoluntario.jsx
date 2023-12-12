53fedf91621617de9b4918228c9e6c9e92eacdf5
Julian <julianlopez4301@gmail.com>
2023-12-12 12:09:14 -0300

    boton form

--- src/components/CardQuieroSerVoluntario/CardQuieroSerVoluntario.jsx
+++ src/components/CardQuieroSerVoluntario/CardQuieroSerVoluntario.jsx
@@ -1,12 +1,25 @@
 import Image from "next/image";
 import ImagenDefault from "@/../public/images/QuieroSerVoluntario/1.png";
-export default function CardQuieroSerVoluntario({ titulo, imagen }) {
+import { FaWpforms } from "react-icons/fa";
+export default function CardQuieroSerVoluntario({ titulo, imagen, form }) {
   return (
-    <section className="w-full max-w-[320px] h-[200px] bg-[#c2c2c2] rounded-md p-6">
+    <section className="w-full max-w-[320px] h-[225px] bg-[#c2c2c2] rounded-md p-6 flex flex-col gap-4">
       <h2 className="text-[#0C6410] text-xl font-semibold text-center">
         {titulo}
       </h2>
       <Image src={imagen ? imagen : ImagenDefault} alt="imagen" />
+
+      <div className="w-full justify-center flex">
+        <a
+          href={form}
+          target="_blank"
+          className="transform transition-all hover:scale-105 duration-300 ease-in-out"
+        >
+          <span className="mx-auto rounded-xl py-[0.35rem] px-4 bg-[#979595]">
+            Quiero inscribirme
+          </span>
+        </a>
+      </div>
     </section>
   );
 }
