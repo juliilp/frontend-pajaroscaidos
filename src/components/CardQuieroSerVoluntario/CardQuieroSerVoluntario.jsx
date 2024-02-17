9cc312ea48e20b2c509229caaf6c474ae6fc01e4
Julian Lopez Padua <julianlopez43013@gmail.com>
2024-02-16 21:50:15 -0300

    fix card

--- src/components/CardQuieroSerVoluntario/CardQuieroSerVoluntario.jsx
+++ src/components/CardQuieroSerVoluntario/CardQuieroSerVoluntario.jsx
@@ -3,11 +3,17 @@ import ImagenDefault from "@/../public/images/QuieroSerVoluntario/1.png";
 import { FaWpforms } from "react-icons/fa";
 export default function CardQuieroSerVoluntario({ titulo, imagen, form }) {
   return (
-    <section className="w-full max-w-[320px] h-[225px] bg-[#c2c2c2] rounded-md p-6 flex flex-col gap-4">
+    <section className="w-full max-w-[350px] h-[350px] bg-[#c2c2c2] rounded-md p-8 flex flex-col gap-4">
       <h2 className="text-[#0C6410] text-xl font-semibold text-center">
         {titulo}
       </h2>
-      <Image src={imagen ? imagen : ImagenDefault} alt="imagen" />
+      <Image
+        src={imagen ? imagen : ImagenDefault}
+        alt="imagen"
+        width={300}
+        height={150}
+        className="h-[200px]"
+      />
 
       <div className="w-full justify-center flex">
         <a
